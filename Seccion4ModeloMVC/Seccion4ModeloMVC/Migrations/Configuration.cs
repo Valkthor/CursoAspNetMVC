992facafc849388328a18c3bb3a4bf2bf7e91901
Valkthor <srojas@contaline.cl>
2020-09-29 23:13:33 -0300

    aplicacion de validaciones a los campos del formulario

---
+++ Seccion4ModeloMVC/Seccion4ModeloMVC/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace Seccion4ModeloMVC.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Seccion4ModeloMVC.Models.EmpDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Seccion4ModeloMVC.Models.EmpDBContext";
+        }
+
+        protected override void Seed(Seccion4ModeloMVC.Models.EmpDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
--- Seccion4ModeloMVC/Seccion4ModeloMVC/Models/Clientes.cs
+++ Seccion4ModeloMVC/Seccion4ModeloMVC/Models/Clientes.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Seccion4ModeloMVC.Models
 {
     public class Clientes
     {
         public int ID { get; set; }
+        
+        // maximo 60 caracteres, y minimo 3
+        [StringLength (60, MinimumLength =3)]
         public string nombre { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display (Name = "Fecha de Alta")]
+        [DataType (DataType.Date)]
         public DateTime FechaAlta { get; set; }
+
+        [Range (18, 75)]
         public int edad { get; set; }
     }
 }
