bd52604ffc6978f8dc51da61af9acf8e3b72fb5b
Valkthor <srojas@contaline.cl>
2020-11-06 18:57:57 -0300

    ejercicio 6 terminado y funcionado, crea registros en bd local

---
+++ Seccion6Formularios/RegistroMVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using RegistroMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistroMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Registro obj)
+        {
+            if (ModelState.IsValid)
+            {
+                Seccion6FormsEntities db = new Seccion6FormsEntities();
+                db.Registroes.Add(obj);
+                db.SaveChanges();
+            }
+            return View(obj);
+        }
+    }
+}
